ccf6b316da3bbe599b149abab41e78717624a37c
xrpusher <60086412+xrpusher@users.noreply.github.com>
2024-03-13 14:50:42 +0300

    Update task_3.c

--- task_3.c
+++ task_3.c
@@ -1,92 +1,101 @@
-#include "ztimer.h"
+#include <stdio.h>
 #include "periph/gpio.h"
 #include "xtimer.h"
 
-#define DEBOUNCE_DELAY 300000
-#define LONG_PRESS 3000000
+#define DEBOUNCE_DELAY 300000   // Задержка для антидребезга
+#define LONG_PRESS 30000      // Время для определения долгого нажатия
 
-static gpio_t green_led = GPIO_PIN(PORT_C, 13);
-static gpio_t yellow_led = GPIO_PIN(PORT_C, 14);
-static gpio_t red_led = GPIO_PIN(PORT_C, 15);
-static gpio_t button_pin = GPIO_PIN(PORT_B, 1);
+// Определение пинов для светодиодов
+static gpio_t red_led_pin = GPIO_PIN(PORT_C, 13);
+static gpio_t yellow_led_pin = GPIO_PIN(PORT_B, 14);
+static gpio_t green_led_pin = GPIO_PIN(PORT_B, 7);
 
+// Определение пина для кнопки
+static gpio_t button_pin = GPIO_PIN(PORT_B, 11);
+
+// Таймеры
 static xtimer_t debounce_timer;
-static uint32_t press_start_time;
-static uint32_t press_duration;
+static xtimer_t led_timer;
 
-static uint32_t green_led_interval = 5000;  // время свечения зеленого света
-static uint32_t yellow_led_interval = 2000; // время свечения желтого света
-static uint32_t red_led_interval = 5000;    // время свечения красного света
-static uint32_t current_phase_time = 0;     // текущее время фазы
-static uint32_t next_green_time = 0;        // время до следующего включения зеленого света
+// Временные метки
+static uint32_t last_green_time = 0;
+static uint32_t current_phase_time = 5000000; // 5 секунд для каждой фазы
 
-enum TrafficLightPhase {
-    GREEN,
-    YELLOW,
-    RED
-} current_phase = RED;
+enum { RED, YELLOW, GREEN } current_light = RED;
 
-void traffic_light_update(void) {
-    gpio_clear(green_led);
-    gpio_clear(yellow_led);
-    gpio_clear(red_led);
+void toggle_leds(void)  {
+    switch (current_light) {
+        case RED:
+            gpio_clear(red_led_pin);
+            gpio_clear(yellow_led_pin);
+            gpio_clear(green_led_pin);
+            current_light = GREEN;
 
-    switch (current_phase) {
+            break;
         case GREEN:
-            gpio_set(green_led);
-            current_phase_time = green_led_interval;
-            current_phase = YELLOW;
+            gpio_set(red_led_pin);
+            gpio_clear(yellow_led_pin);
+            gpio_set(green_led_pin);
+            last_green_time = xtimer_now_usec(); 
+            current_light = YELLOW;
             break;
         case YELLOW:
-            gpio_set(yellow_led);
-            current_phase_time = yellow_led_interval;
-            current_phase = RED;
-            break;
-        case RED:
-            gpio_set(red_led);
-            current_phase_time = red_led_interval;
-            current_phase = GREEN;
+            gpio_set(red_led_pin);
+            gpio_set(yellow_led_pin);
+            gpio_clear(green_led_pin);
+            current_light = RED;
             break;
     }
-
-    next_green_time -= current_phase_time;
-    if (next_green_time < 0) {
-        next_green_time = green_led_interval + yellow_led_interval + red_led_interval;
-    }
-}
-
-void button_handler(void *arg) {
-    (void)arg;
-    uint32_t time_to_green = next_green_time - current_phase_time;
-    printf("Время до зеленого света: %lu мс\n", time_to_green);
-    if (time_to_green > LONG_PRESS && current_phase != GREEN) {
-        printf("Планирование нового переключения на зеленый\n");
-        traffic_light_update();  // немедленное переключение светофора
-    }
+    xtimer_set(&led_timer, current_phase_time);
 }
 
 void debounce_callback(void *arg) {
     (void)arg;
     gpio_irq_enable(button_pin);
-    xtimer_remove(&debounce_timer);
+}
+
+
+void button_handler(void *arg) {
+    (void)arg;
+    uint32_t now = xtimer_now_usec();
+    uint32_t time_since_green;
+
+    if (last_green_time > now) { // Учет переполнения таймера
+        time_since_green = (UINT32_MAX - last_green_time) + now;
+    } else {
+        time_since_green = now - last_green_time;
+    }
+
+    if (current_light != GREEN && time_since_green + LONG_PRESS < current_phase_time) {
+        // Ускоряем переключение на зеленый свет
+        xtimer_set(&led_timer, LONG_PRESS);
+    }
+
+    gpio_irq_disable(button_pin);
+    xtimer_set(&debounce_timer, DEBOUNCE_DELAY);
 }
 
 int main(void) {
-    if (gpio_init(green_led, GPIO_OUT) < 0 || gpio_init(yellow_led, GPIO_OUT) < 0 || gpio_init(red_led, GPIO_OUT) < 0) {
-        printf("Ошибка инициализации светодиодов\n");
-        return 1;
-    }
-    traffic_light_update();
+    // Инициализация светодиодов
+    gpio_init(red_led_pin, GPIO_OUT);
+    gpio_init(yellow_led_pin, GPIO_OUT);
+    gpio_init(green_led_pin, GPIO_OUT);
 
+    // Инициализация кнопки
     if (gpio_init_int(button_pin, GPIO_IN_PU, GPIO_FALLING, button_handler, NULL) < 0) {
         printf("Ошибка инициализации кнопки\n");
         return 1;
     }
+
+    // Настройка таймеров
     debounce_timer.callback = debounce_callback;
+    led_timer.callback = (void (*)(void *))toggle_leds;
+
+    // Начальное состояние светофора
+    toggle_leds();
 
     while (1) {
-        ztimer_sleep(ZTIMER_MSEC, current_phase_time);
-        traffic_light_update();
+        xtimer_sleep(1);
     }
 
     return 0;
